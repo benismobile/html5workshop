beadd5ee8452dd2c98bb46d74f72bc6cc9a16ca8
ide user ide_benismobile <b.butchart@gmail.com>
2012-04-12 18:14:18 +0000

    tidy up featuredetect.js

--- canvasfeaturedetect/featuredetect.js
+++ canvasfeaturedetect/featuredetect.js
@@ -1,56 +1,66 @@
 function init()
 {
 
-var img = new Image(255,255) ;
-img.src = "/images/test1.png"  ;
+var img = new Image(256,256) ;
+img.src = "images/test1.png"  ;
 
 var canvas = document.getElementById("canvasimg") ;
-var canvasContext = newCanvas.getContext("2d") ;
+var canvasContext = canvas.getContext("2d") ; // need 2d context to draw
 var c = canvas.getContext("2d") ;
 
 img.onload = function  (){
         c.drawImage(img, 0, 0);
 };
 
+canvas.addEventListener("mousemove", mousemoveev, false);
 
-newCanvas.onmousemove = function(e){ 
 
-        e.preventDefault() ;
-		drawMode = true ;
+    function mousemoveev(e){ 
+
+        e.stopPropagation();
+        e.preventDefault();
+	
 		
-		var xy = getxy(e, newCanvas ) ;
+		var xy = getxy(e, canvas ) ;  // get mouse postion on screen 
 
+        // redraw the image to get rid of previous highlighting
 		canvasContext.drawImage(img, 0, 0);
 		
-
+        // get the image data for the current mouse position pixel
 		var imgd = c.getImageData(xy.x, xy.y, 1 ,1 ) ;
-		var pix1 = imgd.data ;
+		var pix1 = imgd.data ;  // array of rgba values in image
 		var i = 0 ;
+        var n = 0 ;
+        
+        // extract RGB value for this pixel
 		var red = pix1[i]; // red
-    		var green =  pix1[i+1]; // green
+        var green =  pix1[i+1]; // green
 		var blue =  pix1[i+2]; // blue
-		var w = newCanvas.width;
-		var h = newCanvas.height;
+		
+        var w = canvas.width;
+		var h = canvas.height;
 		var rect = { left : 0, top : 0, width : w, height : h } ;
+        
+        // get the image data for the whole canvas
 		var idata = c.getImageData(rect.left, rect.top, rect.width ,rect.height ) ;
 		var pix = idata.data;
 
 
 		// Loop over each pixel and darken the color of all other pixels in image
-            // that have a different color to the selected pixel
-		for (var i = 0, n = pix.length; i < n; i += 4) {
+        // that have a different color to the selected pixel
+		for (i = 0, n = pix.length; i < n; i += 4) {
 					
 			 var brightness = 0.75 ;
 			 if (	pix[i ] == red &&  pix[i+1] == green && pix[i+2] == blue )
 				brightness = 1.00 ;																				
 			
 			  var r = 0 ;
-			  if ((r = pix[i] * brightness  ) > 255 )
+			  if ((r = pix[i] * brightness  ) > 255 ) // adding brightness should not go over 255
 					pix[i] = 255;
 			  else if (r < 0)
 					pix[i] = 0;
 			  else
- 					pix[i] = r;
+                    pix[i] = r;
 
 			  var g = 0 ;
 			  if ((g = pix[i+1] * brightness  ) > 255 )
@@ -58,16 +68,16 @@ newCanvas.onmousemove = function(e){
 			  else if (g < 0)
 					pix[i+1] = 0;
 			  else
- 					pix[i+1] = g;
+                    pix[i+1] = g;
 						
-			  var h = pix[i+2] * brightness
-			  var h = 0 ;
+			  h = pix[i+2] * brightness ;
+			  h = 0 ;
 			  if ((h = pix[i+2] * brightness  ) > 255 )
 					pix[i+2] = 255;
 			  else if (h < 0)
 					pix[i+2] = 0;
 			  else
- 					pix[i+2] = h;
+                    pix[i+2] = h;
 
 							
 			  
@@ -93,7 +103,7 @@ function getxy(e, o) {
 		var y = e.clientY - bo.y ;									
 		x += document.documentElement.scrollLeft;	//correct for window scroll offset
 		y += document.documentElement.scrollTop;										
-		return { x: x-.5, y: y-.5 }; //-.5 prevents antialiasing of stroke lines
+		return { x: x-0.5, y: y-0.5 }; //-.5 prevents antialiasing of stroke lines
 
 }
 
@@ -116,7 +126,7 @@ function getpos(o) {
 			o = o.offsetParent;
 		}
 	}
-	return { x:x, y:y }
+	return { x:x, y:y };
 }
 
 
